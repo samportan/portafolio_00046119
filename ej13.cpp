136bb851afd3386349b15fd8948972f929a5dbfc
samportan <50893014+samportan@users.noreply.github.com>
2019-09-09 12:56:44 -0600

    Labo #2

---
+++ ej13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int func(int);
+
+int main(){
+    int n, sum=0;
+    cin>>n;
+    
+    for(int i=0;i<=n;i++){
+        sum=sum+i;
+    }
+    cout<<sum;
+    
+    
+    return 0;
+}
+
+
